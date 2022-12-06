4c246034f77c8fc5b2bc8123b92351d1cc36a6b2
yoonsuin <115388103+yoonsuin@users.noreply.github.com>
2022-12-06 20:44:43 +0900

    ADD

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -20,31 +20,33 @@ function Login() {
   console.log('password:', password);
 
   return (
-    <div className="login_wrap">
-      <input
-        className="input_email"
-        type="text"
-        value={email}
-        placeholder="이메일을 입력해주세요"
-        onChange={onChangeEmail}
-      />
+    <div className="login">
+      <div className="login_wrap">
+        <input
+          className="input_email"
+          type="text"
+          value={email}
+          placeholder="이메일을 입력해주세요"
+          onChange={onChangeEmail}
+        />
 
-      <input
-        className="input_password"
-        type="password"
-        value={password}
-        placeholder="비밀번호를 입력해주세요"
-        onChange={onChangePassword}
-      />
-      <button className="signin_button">로그인</button>
-      <button className="signup_button">회원가입</button>
+        <input
+          className="input_password"
+          type="password"
+          value={password}
+          placeholder="비밀번호를 입력해주세요"
+          onChange={onChangePassword}
+        />
+        <button className="signin_button">로그인</button>
+        <button className="signup_button">회원가입</button>
 
-      <div className="find_wrap">
-        <div>
-          <a class="find_email">이메일 찾기</a>
-        </div>
-        <div>
-          <a class="find_askpassword">비밀번호 찾기</a>
+        <div className="find_wrap">
+          <div>
+            <a class="find_email">이메일 찾기</a>
+          </div>
+          <div>
+            <a class="find_askpassword">비밀번호 찾기</a>
+          </div>
         </div>
       </div>
     </div>
