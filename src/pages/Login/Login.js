a25844440918ec48b104ca0c28b9f6f399ee466f
yoonsuin <115388103+yoonsuin@users.noreply.github.com>
2022-12-06 21:38:34 +0900

    input handler 함수 합치기 오류 -멘토님 필요

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,21 +3,23 @@ import { Navigate, useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const disabled = !(email.includes('@') && password.length >= 5);
-
-  const onChangeEmail = e => {
-    setEmail(e.target.value);
-    console.log(email);
+  const LoginInput = () => {
+    const [inputValues, setInputValues] = useState({
+      email: '',
+      password: '',
+    });
   };
-  console.log('email:', email);
 
-  const onChangePassword = e => {
-    setPassword(e.target.value);
+  const handleInput = event => {
+    const { name, value } = event.target;
+    setInputValues({ ...inputValues, [name]: value });
   };
-  console.log('password:', password);
+
+  const activateButton = () => {
+    const isValid =
+      LoginInput.email.includes('@') && LoginInput.password.length >= 5;
+    setIsValid(isValid);
+  };
 
   return (
     <div className="login">
@@ -25,19 +27,21 @@ function Login() {
         <input
           className="input_email"
           type="text"
-          value={email}
           placeholder="이메일을 입력해주세요"
-          onChange={onChangeEmail}
+          value={inputValues}
+          onChange={handleInput}
         />
 
         <input
           className="input_password"
           type="password"
-          value={password}
           placeholder="비밀번호를 입력해주세요"
-          onChange={onChangePassword}
+          value={inputValues}
+          onChange={handleInput}
         />
-        <button className="signin_button">로그인</button>
+        <button className="signin_button" onClick={activateButton}>
+          로그인
+        </button>
         <button className="signup_button">회원가입</button>
 
         <div className="find_wrap">
