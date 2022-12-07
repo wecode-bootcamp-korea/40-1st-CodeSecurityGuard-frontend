62d19fa6958f0fe83957f770ac7612c2c5116413
김채영 <cccodud@gmail.com>
2022-12-07 16:55:52 +0900

    ADD: Nav바 레이아웃 구현완료

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Header from './components/Header/Header';
+import Nav2 from './components/Nav/Nav2';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      {/* <Header />
+      <Nav /> */}
+      <Nav2 />
       <Routes>
+        <Route path="/" element={<ProductDetail />} />
         <Route path="/productdetail" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
