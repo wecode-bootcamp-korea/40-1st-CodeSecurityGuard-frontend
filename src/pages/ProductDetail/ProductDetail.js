a82bde0d8ffdefeb35c29347d1aef4b2227cfaa9
NA <na@NAui-MacBookAir.local>
2022-12-09 16:19:34 +0900

    Add: 레이아웃 구현

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -7,7 +7,9 @@ const ProductDetail = () => {
       {INFOS.map(INFO => (
         <div key={INFO.id} className="prodcutDetailContentsBox">
           <div className="productDetailArea">
-            <div className="proudctDetailImageArea" />
+            <div className="proudctDetailImageArea">
+              <img alt="productImage" src={INFO.thumbnail_image_url} />
+            </div>
             <div className="ProductDetailInfoArea">
               <h3 className="productDetailBrandName">{INFO.brandid}</h3>
               <h1 className="productDetailProductName">{INFO.name}</h1>
@@ -16,18 +18,21 @@ const ProductDetail = () => {
               </span>
               <div className="productDetailPriceReviewBox">
                 <div className="productDetailPrice">{INFO.price}원</div>
-                <div className="productDetailReview">⭐️(22)</div>
               </div>
               <div className="productDetailQuantityBox">
                 <div className="productDetailQuantity">
                   <button className="downQuantityBtn">-</button>
-                  <input type="text" value="1" />
+                  <input
+                    className="productDetailQuantityValue"
+                    type="text"
+                    value="1"
+                  />
                   <button className="upQuantityBtn">+</button>
                 </div>
               </div>
-              <div className="productDetailTotalPrice">
+              <div className="productDetailTotalPriceBox">
                 <span>총 상품금액:</span>
-                <span className="productDetailPrice">{INFO.price}원</span>
+                <p className="productDetailTotalPrice">{INFO.price}원</p>
               </div>
               <div className="productDetailBtnBox">
                 <button className="productDetailCartButton">장바구니</button>
@@ -50,5 +55,7 @@ const INFOS = [
     name: '히알루론산 수분 크림',
     description: '속건조를 잡아주는 8중 복합 히알루론산 수분 크림',
     price: '8,900',
+    thumbnail_image_url:
+      'https://images.unsplash.com/photo-1638609927040-8a7e97cd9d6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
   },
 ];
