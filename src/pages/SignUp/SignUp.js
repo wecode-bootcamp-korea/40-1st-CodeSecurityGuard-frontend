25f968f03577ecd901fe1e46ef4f515112970bbb
yoonsuin <115388103+yoonsuin@users.noreply.github.com>
2022-12-09 14:02:58 +0900

    ADD : 회원가입 레이아웃 구현

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -4,29 +4,28 @@ import './SignUp.scss';
 
 const SignUp = () => {
   return (
-    <div className="signUp">
+    <div className="signUpWrap">
       <div className="signupTop">
         <h2 className="signupTitle">회원가입</h2>
         <h3 className="signupSubtitle">기본정보</h3>
         <p className="signupRequired">필수입력사항</p>
       </div>
+      <div className="userInfo">
+        {USER_FORM.map(info => {
+          return (
+            <div className="form" key={info.id}>
+              <div className="formTitle">{info.formTitle}</div>
+              <input className="formInput" placeholder={info.placeholder} />
+            </div>
+          );
+        })}
+      </div>
 
-      <div className="userInfo" />
+      <div className="PhonenumberWrap">
+        <div className="phonenumberTitle">휴대전화</div>
 
-      {USER_FORM.map(info => {
-        return (
-          <div className="form" key={info.id}>
-            <div className="formTitle">{info.formTitle}</div>
-            <input className="formInput" placeholder={info.placeholder} />
-          </div>
-        );
-      })}
-
-      <div className="signUpInputMobile">
-        <div className="phonNumberTitle">휴대전화</div>
-
-        <div className="mobileInfo">
-          <select className="mobileFirst">
+        <div className="phonenumberInfo">
+          <select className="phonenumberFirst">
             <option value="010">010</option>
             <option value="010">011</option>
             <option value="010">016</option>
@@ -36,14 +35,14 @@ const SignUp = () => {
           </select>
           -
           <input
-            className="mobileCenter"
+            className="phonenumberCenter"
             type="text"
             placeholder="0000"
             maxLength="4"
           />
           -
           <input
-            className="mobileLast"
+            className="phonenumberLast"
             type="text"
             placeholder="0000"
             maxLength="4"
@@ -59,8 +58,8 @@ const SignUp = () => {
         <label>동의합니다 </label>
       </div>
 
-      <div className="signupBox">
-        <button className="signupButton">가입하기</button>
+      <div className="submitWrap">
+        <button className="submitButton ">가입하기</button>
       </div>
     </div>
   );
@@ -77,5 +76,4 @@ const USER_FORM = [
     placeholder: '비밀번호를 한번 더 입력해주세요',
   },
   { id: 4, formTitle: '이름', placeholder: '실명으로 기입해주세요' },
-  { id: 5, formTitle: '주소', placeholder: '시 / 도 / 군・구 / 상세주소' },
 ];
