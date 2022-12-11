53a4011469b7eb687d0740c39c81ea648d167616
김채영 <cccodud@gmail.com>
2022-12-11 19:27:10 +0900

    Add: nav bar 드롭다운 카테고리 기능 구현 - nav bar 왼쪽 하단 카테고리 탭 hover 시 메인메뉴 카테고리 컴포넌트 구현 - 메인메뉴와 서브 카테고리 id 일치 시 서브 카테고리 컴포넌트 구현

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './pages/Footer/Footer';
 import Login from './pages/Login/Login';
+import Nav from './components/Nav/Nav';
+import MainCarousel from './pages/ProductList/MainPage/MainCarousel';
+import ProductCarousel from './pages/ProductList/MainPage/ProductCarousel';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Product from './pages/ProductList/Product/Product';
 import ProductCard from './pages/ProductList/Product/ProductCard';
@@ -10,6 +13,9 @@ import ProductList from './pages/ProductList/ProductList';
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
+      <MainCarousel />
+      <ProductCarousel />
       <Routes>
         <Route path="/productdetail" element={<ProductDetail />} />
         <Route path="/product" element={<Product />} />
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,59 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.scss';
+import CategoryList from '../CategoryList/CategoryList';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="navBox">
-      <header>
-        <div className="headerElement">
-          <ul className="headerLink">
-            <li className="signUpLink">
-              <Link to="#">회원가입</Link>
-            </li>
-            <li className="loginLink">
-              <Link to="#">로그인</Link>
-            </li>
-          </ul>
-          <form className="searchBarForm">
-            <div className="weselyLogo">
-              <li>
-                <Link to="#">WESELY</Link>
+    <nav className="nav">
+      <div className="navBox">
+        <header>
+          <div className="headerElement">
+            <ul className="headerLink">
+              <li className="signUpLink">
+                <Link to="#">회원가입</Link>
               </li>
+              <li className="loginLink">
+                <Link to="#">로그인</Link>
+              </li>
+            </ul>
+            <form className="searchBarForm">
+              <div className="weselyLogo">
+                <Link to="/Main">
+                  <img src="/images/WESELY-logo.png" alt="WESELY" />
+                </Link>
+              </div>
+              <div className="headerSearchBox">
+                <input id="search" className="searchInput" type="text" />
+                <Link to="#">
+                  <img src="/images/loupe.png" alt="돋보기" />
+                </Link>
+              </div>
+              <div className="cartButtonWrapper">
+                <Link to="#">
+                  <img src="/images/shopping-cart.png" alt="장바구니" />
+                </Link>
+              </div>
+            </form>
+          </div>
+        </header>
+        <div className="navWrapper">
+          <div
+            className="categoryWrapper"
+            onMouseEnter={() => setIsOpen(true)}
+            onMouseLeave={() => setIsOpen(false)}
+          >
+            <div className="navLeft">
+              <img src="/images/menu.png" alt="메뉴" />
+              <span>카테고리</span>
             </div>
-            <div className="headerSearchBox">
-              <input id="search" className="searchInput" type="text" />
-              <Link to="#">
-                <img src="/images/loupe.png" alt="돋보기" />
-              </Link>
-            </div>
-            <div className="cartButtonWrapper">
-              <Link to="#">
-                <img src="/images/shopping-cart.png" alt="장바구니" />
-              </Link>
-            </div>
-          </form>
+            <div className="navCategory">{isOpen && <CategoryList />}</div>
+          </div>
+          <div className="tabMenuWrapper">
+            <ul className="tabMenu">
+              <li className="recommendLink">
+                <Link to="#">추천</Link>
+              </li>
+              <li className="newProductLink">
+                <Link to="#">신제품</Link>
+              </li>
+              <li className="bestProductLink">
+                <Link to="#">베스트</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="rightMenuWrapper" />
         </div>
-      </header>
-      <div className="navWrapper">
-        <div className="navLeft">
-          <img src="/images/menu.png" alt="메뉴" />
-          <span>카테고리</span>
-        </div>
-        <div className="tabMenuWrapper">
-          <ul className="tabMenu">
-            <li className="recommendLink">
-              <Link to="#">추천</Link>
-            </li>
-            <li className="newProductLink">
-              <Link to="#">신제품</Link>
-            </li>
-            <li className="bestProductLink">
-              <Link to="#">베스트</Link>
-            </li>
-          </ul>
-        </div>
-        <div className="rightMenuWrapper" />
+        <div className="navCategory">{/* <CategoryList /> */}</div>
       </div>
     </nav>
   );
