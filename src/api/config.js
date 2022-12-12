af702e05dc6d312658d26045bda0381336c0eec9
yoonsuin <115388103+yoonsuin@users.noreply.github.com>
2022-12-12 18:36:38 +0900

    Refactor : 멘토리뷰반영 &  api 정리 완료

--- src/api/config.js
+++ src/api/config.js
@@ -2,8 +2,17 @@
 export const BASE_URL = 'http://10.58.52.118:8000';
 
 export const api = {
-  login: `${BASE_URL}/users/signin`,
+  signin: `${BASE_URL}/users/signin`,
   signup: `${BASE_URL}/users/signup`,
+  //조회: "keywordInQuotations"가 (name || description) 안에 들어간 모든 제품 정보
+  search: `${BASE_URL}/search?keyword="desc1"`,
+  //조회: 모든 제품 정보
   list: `${BASE_URL}/products`,
+  //조회: 제품 한개의 정보
+  product: `${BASE_URL}/products/1`,
+  //조회: 카테고리 한가지의 모든 제품 정보
+  category: `${BASE_URL}/category/3`,
+  //조회: 서브-카테고리 한가지의 모든 제품 정보
+  subcategory: `${BASE_URL}/subCategory/3`,
   detail: id => `${BASE_URL}/product/${id}`,
 };
