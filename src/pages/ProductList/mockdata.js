0b228d070ed3e36b3ed1a44db38dc36a4b5ee9eb
unknown <shinch8803@gmail.com>
2022-12-13 14:43:58 +0900

    modify : productList 충돌해결

---
+++ src/pages/ProductList/mockdata.js
@@ -0,0 +1,9 @@
+const CATEGORY_INFO_LIST = [
+  { id: 1, name: '전체보기', url: 1 },
+  { id: 2, name: '활력충전, 면역', url: 2 },
+  { id: 3, name: '눈/혈행 건강', url: 3 },
+  { id: 4, name: '뼈, 신경, 근육', url: 4 },
+  { id: 5, name: '장/간 건강', url: 5 },
+];
+
+export default CATEGORY_INFO_LIST;
